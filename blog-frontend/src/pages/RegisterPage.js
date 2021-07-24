08bd6d18cf0a2f8fe43f1799f4756687d9365ee8
ykss <yukyeongsang@gmail.com>
2021-07-24 23:03:06 +0900

    Fix : delete unused module

--- blog-frontend/src/pages/RegisterPage.js
+++ blog-frontend/src/pages/RegisterPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import AuthForm from '../components/auth/AuthForm';
 import AuthTemplate from '../components/auth/AuthTemplate';
 import RegisterForm from '../containers/auth/RegisterForm';
 
