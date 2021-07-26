053dbdb3ba5ac5bf1f686517359d8c9690f1090e
ykss <yukyeongsang@gmail.com>
2021-07-27 00:34:18 +0900

    Add : userSaga 적용

--- blog-frontend/src/modules/index.js
+++ blog-frontend/src/modules/index.js
@@ -1,15 +1,17 @@
 import { combineReducers } from 'redux';
 import auth, { authSaga } from './auth';
 import loading from './loading';
+import user, { userSaga } from './user';
 import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
   auth,
   loading,
+  user,
 });
 
 export function* rootSaga() {
-  yield all([authSaga()]);
+  yield all([authSaga(), userSaga()]);
 }
 
 export default rootReducer;
